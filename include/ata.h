8847927e6b28dd128c76c6d586389a2ae8bab754
Mert Can Altin <mertgold60@gmail.com>
2026-07-17 02:14:44 +0300

    chore: begin 1.1.0

--- include/ata.h
+++ include/ata.h
@@ -8,13 +8,13 @@
 #include <variant>
 #include <vector>
 
-#define ATA_VERSION "1.0.2"
+#define ATA_VERSION "1.1.0"
 
 namespace ata {
 
 inline constexpr uint32_t VERSION_MAJOR = 1;
-inline constexpr uint32_t VERSION_MINOR = 0;
-inline constexpr uint32_t VERSION_REVISION = 2;
+inline constexpr uint32_t VERSION_MINOR = 1;
+inline constexpr uint32_t VERSION_REVISION = 0;
 
 inline constexpr std::string_view version() noexcept {
   return ATA_VERSION;
