f8b19ba94abb20f20164104af84ce42cd2f69c5d
Mert Can Altin <mertgold60@gmail.com>
2026-08-28 01:19:10 +0300

    chore: bump to 1.9.0

--- include/ata.h
+++ include/ata.h
@@ -8,13 +8,13 @@
 #include <variant>
 #include <vector>
 
-#define ATA_VERSION "1.8.2"
+#define ATA_VERSION "1.9.0"
 
 namespace ata {
 
 inline constexpr uint32_t VERSION_MAJOR = 1;
-inline constexpr uint32_t VERSION_MINOR = 8;
-inline constexpr uint32_t VERSION_REVISION = 2;
+inline constexpr uint32_t VERSION_MINOR = 9;
+inline constexpr uint32_t VERSION_REVISION = 0;
 
 inline constexpr std::string_view version() noexcept {
   return ATA_VERSION;
